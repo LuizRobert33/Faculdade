b9021325c4fcc28cf97562f0d4d2c04e44478431
Luiz Roberto <110065527+LuizRobert33@users.noreply.github.com>
2023-03-29 18:41:55 -0300

    .

--- Maratona de Programação/letraK.py
+++ Maratona de Programação/letraK.py
@@ -25,3 +25,21 @@ I##sso ´e completamente inaceit´avel: nenhuma oportunidade de comer de gra¸ca
 ##Imprima uma ´unica linha contendo um ´unico caractere. Se vocˆe consegue descansar durante o vˆoo
 ##e ainda assim comer todas as refei¸c˜oes, imprima ‘Y’; caso contr´ario, imprima ‘N’.
 
+t, d, m = map(int, input().split())
+
+refeicoes = []
+for i in range(m):
+    refeicoes.append(int(input()))
+
+i = 0
+while i < len(refeicoes) and refeicoes[i] - t >= i:
+    i += 1
+
+j = i
+while j < len(refeicoes) and refeicoes[j] - t < j:
+    j += 1
+
+if i == j:
+    print('Y')
+else:
+    print('N')
