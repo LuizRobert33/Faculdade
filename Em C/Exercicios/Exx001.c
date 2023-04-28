155b5f1afeeb7c743d723db797ade381b73f8f9a
Luiz Roberto <110065527+LuizRobert33@users.noreply.github.com>
2023-04-27 21:46:34 -0300

    update

---
+++ Em C/Exercicios/Exx001.c
@@ -0,0 +1,12 @@
+// Programa de cálculo de média
+#include <stdio.h>
+
+int main(){
+    int N1,N2,Total;
+    printf("Digite um numero: ");
+    scanf("%d",&N1);
+    printf("Digite o segundo numero: ");
+    scanf("%d",&N2);
+    Total = N1 + N2;
+    printf("a soma total é %d",Total);
+}
