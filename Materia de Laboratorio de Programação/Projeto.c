0eda0eebfb3b89612d7f4b11f0129c4a3c41f4f9
Luiz Roberto <110065527+LuizRobert33@users.noreply.github.com>
2023-12-05 10:58:26 -0300

    Update

--- Materia de Laboratorio de Programação/Projeto.c
+++ Materia de Laboratorio de Programação/Projeto.c
@@ -3,10 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define maximo_times 10
-#define maximo_jogadores 18
-#define minimo_jogadores 11
-#define estrangeiros 3
+#define MAX_TIMES 10
+#define MAX_JOGADORES 18
+#define MIN_JOGADORES 11
+#define MAX_ESTRANGEIROS 3
 
 typedef struct {
     char nome[50];
@@ -16,46 +16,44 @@ typedef struct {
 typedef struct {
     char nome_campeonato[50];
     char nome_time[50];
-    Jogador jogadores[maximo_jogadores];
+    Jogador jogadores[MAX_JOGADORES];
     int numero_jogadores;
     int numero_estrangeiros;
 } Time;
 
 void adicionarJogador(Time *time) {
     int contador = time->numero_jogadores + 1;
-    if (contador <= maximo_jogadores) {
-        printf("Informe o nome do o jogador: ", contador);
-        scanf("%s", time->jogadores[time->numero_jogadores].nome);
+    if (contador <= MAX_JOGADORES) {
+        printf("Informe o nome do jogador: ");
+        scanf(" %[^\n]", time->jogadores[time->numero_jogadores].nome);
 
-        printf("O jogador e estrangeiro? (1 para sim, 0 para nao): ");
+        printf("O jogador e estrangeiro? (1 para sim, 0 para não): ");
         scanf("%d", &time->jogadores[time->numero_jogadores].estrangeiro);
 
         if (time->jogadores[time->numero_jogadores].estrangeiro) {
-            time->numero_estrangeiros++;
-            if (time->numero_estrangeiros > estrangeiros) {
-                printf("Erro: Muitos jogadores estrangeiros. Por favor, tente novamente.\n");
-                time->numero_estrangeiros--;
+            if (time->numero_estrangeiros < MAX_ESTRANGEIROS) {
+                time->numero_estrangeiros++;
             } else {
-                time->numero_jogadores++;
+                printf("Erro: Muitos jogadores estrangeiros. Por favor, tente novamente.\n");
+                return;
             }
-        } else {
-            time->numero_jogadores++;
         }
+        
+        time->numero_jogadores++;
     } else {
         printf("Erro: Muitos jogadores. Nao e possivel cadastrar mais jogadores.\n");
     }
 }
 
 void cadastrarTime(Time *times, int *num_times) {
-    if (*num_times < maximo_times) {
+    if (*num_times < MAX_TIMES) {
         if (*num_times == 0) {
-            
             printf("Informe o nome do campeonato: ");
-            scanf("%s", times[*num_times].nome_campeonato);
+            scanf(" %[^\n]", times[*num_times].nome_campeonato);
         }
 
         printf("Informe o nome do time: ");
-        scanf("%s", times[*num_times].nome_time);
+        scanf(" %[^\n]", times[*num_times].nome_time);
 
         times[*num_times].numero_jogadores = 0;
         times[*num_times].numero_estrangeiros = 0;
@@ -63,19 +61,19 @@ void cadastrarTime(Time *times, int *num_times) {
         int opcao;
         do {
             adicionarJogador(&times[*num_times]);
-            printf("Deseja adicionar outro jogador? (1 para sim, 0 para nao): ");
+            printf("Deseja adicionar outro jogador? (1 para sim, 0 para não): ");
             scanf("%d", &opcao);
         } while (opcao);
 
-        if (times[*num_times].numero_jogadores < minimo_jogadores || times[*num_times].numero_jogadores > maximo_jogadores) {
-            printf("Erro: O time deve ter entre %d e %d jogadores. Seu time tem menos que isso entao nao sera possivel registralo no torneio.\n", minimo_jogadores, maximo_jogadores);
+        if (times[*num_times].numero_jogadores < MIN_JOGADORES || times[*num_times].numero_jogadores > MAX_JOGADORES) {
+            printf("Erro: O time deve ter entre %d e %d jogadores. Seu time tem menos que isso entao nao sera possivel registra-lo no torneio.\n", MIN_JOGADORES, MAX_JOGADORES);
             
             *num_times -= 1;
         } else {
             (*num_times)++;
         }
     } else {
-        printf("Erro: Muitos times. Nao e possivel cadastrar mais times.\n");
+        printf("Erro: Muitos times. Nao e possível cadastrar mais times.\n");
     }
 }
 
@@ -112,21 +110,21 @@ void mostrarTimes(Time *times, int num_times) {
 
 int main() {
     printf("BEM VINDO AO PROGRAMA PARA CRIAR E CADASTRAR TIMES NA SUA PROPRIA COMPETICAO. DIVIRTA-SE\n");
-    printf("Vamos a Regras:\n");
-    printf("1 - voce pode cadastra no maximo 10 equipes\n");
+    printf("Vamos as Regras:\n");
+    printf("1 - Voce pode cadastrar no maximo 10 equipes\n");
     printf("2 - Cada equipe pode ter no minimo 11 jogadores e no maximo 18.\n");
-    printf("3 - cada equipe pode ter no maximo 3 jogadores estrageiros.\n");
-    printf("4 - em caso de mais de 3 jogadores estrageiros o Usuario podera tentar cadastra novamente um jogador com tanto que esse nao seja estrageiro.\n");
-    Time times[maximo_times];
+    printf("3 - Cada equipe pode ter no maximo 3 jogadores estrangeiros.\n");
+    printf("4 - Em caso de mais de 3 jogadores estrangeiros, o usuario podera tentar cadastrar novamente um jogador, desde que este nao seja estrangeiro.\n");
+
+    Time times[MAX_TIMES];
     int num_times = 0;
 
     int opcao;
     do {
         cadastrarTime(times, &num_times);
-        printf("Deseja cadastrar outro time? (1 para sim, 0 para nao): ");
+        printf("Deseja cadastrar outro time? (1 para sim, 0 para não): ");
         scanf("%d", &opcao);
     } while (opcao);
-
 
     salvar(times, num_times);
 
