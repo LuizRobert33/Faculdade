352157e6315670a52576e7497153cf55a540daad
Luiz Roberto <110065527+LuizRobert33@users.noreply.github.com>
2025-06-22 00:23:25 -0300

    Update

--- Sistemas_Operacionais/threads02.c
+++ Sistemas_Operacionais/threads02.c
@@ -1,74 +1,88 @@
-// threads02.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <time.h>
 
-#define TAM_VETOR 5
-#define PRODUCOES 10
+#define TAM_VETOR 3
+#define TOTAL_THREADS 5
 
-int buffer[TAM_VETOR];
+int buffer[TAM_VETOR] = {0};
 int in = 0, out = 0;
-int count = 0; // itens no buffer
-int turn = 0;  // 0 = produtor, 1 = consumidor
+int turn = 0;
 
-pthread_mutex_t lock;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void imprimir_vetor() {
+    for (int i = 0; i < TAM_VETOR; i++) {
+        printf("%d ", buffer[i]);
+    }
+    printf("\n");
+}
 
 void *produtor(void *arg) {
-    int i;
-    for (i = 0; i < PRODUCOES; i++) {
+    int id = *(int *)arg;
+
+    while (1) {
         while (turn != 0); // espera sua vez
+        pthread_mutex_lock(&mutex);
 
-        pthread_mutex_lock(&lock);
-
-        if (count < TAM_VETOR) {
+        for (int i = 0; i < TAM_VETOR; i++) {
             int valor = rand() % 100;
             buffer[in] = valor;
+            printf("produtor %d: produziu: %d na posicao: %d\n", id, valor, in + 1);
             in = (in + 1) % TAM_VETOR;
-            count++;
-            printf("Produtor produziu %d\n", valor);
         }
+        imprimir_vetor();
+        usleep(300000); // pausa
 
-        turn = 1; // passa a vez ao consumidor
-        pthread_mutex_unlock(&lock);
-        usleep(100000); // atraso para simular produção
+        turn = 1; // passa a vez para consumidor
+        pthread_mutex_unlock(&mutex);
+        break;
     }
+
     pthread_exit(NULL);
 }
 
 void *consumidor(void *arg) {
-    int i;
-    for (i = 0; i < PRODUCOES; i++) {
+    int id = *(int *)arg;
+
+    while (1) {
         while (turn != 1); // espera sua vez
+        pthread_mutex_lock(&mutex);
 
-        pthread_mutex_lock(&lock);
-
-        if (count > 0) {
+        for (int i = 0; i < TAM_VETOR; i++) {
             int valor = buffer[out];
+            printf("consumidor %d: consumiu: %d na posicao: %d\n", id, valor, out + 1);
+            buffer[out] = 0;
             out = (out + 1) % TAM_VETOR;
-            count--;
-            printf("Consumidor consumiu %d\n", valor);
         }
+        imprimir_vetor();
+        usleep(300000); // pausa
 
-        turn = 0; // passa a vez ao produtor
-        pthread_mutex_unlock(&lock);
-        usleep(100000); // atraso para simular consumo
+        turn = 0; // volta a vez para produtor
+        pthread_mutex_unlock(&mutex);
+        break;
     }
+
     pthread_exit(NULL);
 }
 
 int main() {
-    pthread_t t_produtor, t_consumidor;
+    srand(time(NULL));
 
-    pthread_mutex_init(&lock, NULL);
+    pthread_t produtores[TOTAL_THREADS];
+    pthread_t consumidores[TOTAL_THREADS];
+    int ids[TOTAL_THREADS];
 
-    pthread_create(&t_produtor, NULL, produtor, NULL);
-    pthread_create(&t_consumidor, NULL, consumidor, NULL);
+    for (int i = 0; i < TOTAL_THREADS; i++) {
+        ids[i] = i + 1;
+        pthread_create(&produtores[i], NULL, produtor, &ids[i]);
+        pthread_join(produtores[i], NULL);
 
-    pthread_join(t_produtor, NULL);
-    pthread_join(t_consumidor, NULL);
-
-    pthread_mutex_destroy(&lock);
+        pthread_create(&consumidores[i], NULL, consumidor, &ids[i]);
+        pthread_join(consumidores[i], NULL);
+    }
 
     return 0;
 }
